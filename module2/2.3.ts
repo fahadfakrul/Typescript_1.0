b106b10d64b70157330c9e2435c575025d3d34b7
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-11-03 08:52:48 +0600

    generic type

---
+++ module2/2.3.ts
@@ -0,0 +1,38 @@
+{
+//
+//generic types
+type GenericArray<T> = Array<T>
+// const rollNumbers : number[] = [3,6,8]
+const rollNumbers : Array<number> = [3,6,8]
+
+// const mentors : string[] = ["A", "B", "C", "D", "E", "F"]
+const mentors : GenericArray<string> = ["A", "B", "C", "D", "E", "F"]
+
+// const boolArray: boolean[] = [true, false, false, false, false]
+const boolArray: GenericArray<boolean> = [true, false, false, false, false]
+//
+
+const user : GenericArray<{name: string; age: number}> = [
+    {
+        name: 'John Doe',
+        age: 25,
+        
+    },
+    {
+        name: 'Jane Doe',
+        age: 30,
+       
+    }
+]
+
+
+// generic tuple function
+type GenericTuple<X,Y> = [X,Y]
+const manush : GenericTuple<string, string> = ["John", "Jane"]
+
+const UserWithId : GenericTuple<number,{name:string, email: string}> = [1234, {name: 'Fred', email: 'fred@example.com'}]
+const add = (x: number, y: number) => x + y;
+
+add(30 , 290)
+
+}
